9059293393b46db1416bbf2fe0aa6b83f0307fd4
Dylan Chrysochou <dylan.chrysochou@gmail.com>
2021-05-26 13:15:36 -0230

    Add some more mantras/greetings and adjust styles

--- ux/src/components/utils/greetingUtil.jsx
+++ ux/src/components/utils/greetingUtil.jsx
@@ -6,5 +6,13 @@ export const greetings = {
 
 const greetingList = [
   "Good morning",
-  "Good afternoon"
+  "Good afternoon",
+  "Release the things out of your control",
+  "You are whole, you are creative, you are capable of change",
+  "This too shall pass",
+  "You are enough",
+  "With change comes opportunity",
+  "You are stronger than your excuses",
+  "Don't postpone joy",
+  "Progress is more important than perfection"
 ];
