31ce918a20dc027a27011f0ce5c38994e927d442
Dylan Chrysochou <dylan.chrysochou@gmail.com>
2021-06-01 10:27:36 -0230

    Add styling to delete buttons

--- ux/src/components/todo/Tab.jsx
+++ ux/src/components/todo/Tab.jsx
@@ -8,11 +8,9 @@ function Tab(props) {
   }
 
   return (
-    <div id={props.id}>
+    <div id={props.id} className="todo-tab-container" onClick={switchList}>
       <button className="tab-delete" onClick={props.handleDelete}><FaTrashAlt/></button>
-      <div 
-        className={`todo-tab ${props.active ? "active" : ""}`}
-        onClick={switchList}>
+      <div className={`todo-tab ${props.active ? "active" : ""}`}>
           {props.name}
       </div>
     </div>
